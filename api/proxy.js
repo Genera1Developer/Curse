1bdabbfd21c99bd00e3b4a449939f6eb59f380c8
Brick 🧱 <wanted.glitch@gmail.com>
2025-02-21 22:32:59 -0500

    Thx oreo!  Chrome user agent fix

--- api/proxy.js
+++ api/proxy.js
@@ -49,7 +49,7 @@ app.get('/api/proxy.js', async (req, res) => {
         const response = await axios.get(q, {
             responseType: 'arraybuffer',
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.0 Safari/537.3', //49.0.0 to fix some things with YouTube and allat
                 'Referer': q,
                 'Accept': req.headers['accept'] || '*/*',
                 'Accept-Language': req.headers['accept-language'] || 'en-US,en;q=0.9',
