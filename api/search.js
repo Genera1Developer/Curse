1503b6989fe8ce58a127711025b1f006f8df0d43
Oreo <the55thone@gmail.com>
2025-02-27 16:26:06 -0800

    test

--- api/search.js
+++ api/search.js
@@ -57,7 +57,7 @@ export default async function handler(req, res) {
             (match, url) => `${type}="/api/proxy.js?q=${encodeURIComponent(searxInstance + url)}"`
           );
           chunkStr = chunkStr.replace(
-            new RegExp(`${type}=["'](https?:\/\/[^"']+)["']`, 'gi'), //HOLY GOSH, i love regex
+            new RegExp(`${type}=["'](https?:\/\/[^"']+)["']`, 'gi'), //HOLY SHIT, i love regex
             (match, url) => `${type}="/api/proxy.js?q=${encodeURIComponent(url)}"`
           );
         });
