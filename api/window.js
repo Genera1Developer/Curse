6b40e9ebd4d68c9825c81d76fb688b0eaff40f46
Brick 🧱 <wanted.glitch@gmail.com>
2025-03-07 17:33:20 -0500

    Update and rename proxy.js to window.js

--- api/window.js
+++ api/window.js
@@ -133,7 +133,7 @@ app.get('/api/proxy.js', async (req, res) => {
 app.use(express.static('public'));
 // im quoting that⬆️
 // you fucker.                                                                                                                                                                                 you aint nothing but a broke fein lmaoooooooooooooooooooooooooooooo
-
+// luh u too pooks
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
